be570fcf17cc86fcc77d5d0b621e978b522cb43c
v2future <401104212@qq.com>
2021-03-28 15:04:56 +0800

    Create Aes.java

---
+++ btc-study/src/test/java/info/block123/btc/core/Aes.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: AES
+ * Author:   林森
+ * Date:     2019/7/18 10:45
+ * Description: AES加密解密
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package info.block123.btc.core;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.security.Key;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+
+public class AES {
+	
+	  public static final String DEFAULT_KEY = "test";
+
+    public static String DES = "AES"; // optional value AES/DES/DESede
+    public static String CIPHER_ALGORITHM = "AES"; // optional value AES/DES/DESede
+
+    private static Key getKey(String strKey) {
+        try {
+            if (strKey == null) {
+                strKey = "";
+            }
+            KeyGenerator _generator = KeyGenerator.getInstance("AES");
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+            secureRandom.setSeed(strKey.getBytes());
+            _generator.init(128, secureRandom);
+            return _generator.generateKey();
+        } catch (Exception e) {
+            throw new RuntimeException(" 初始化密钥出现异常 ");
+        }
+    }
+
+    public static String encode(String key, String data) {
+    	try {
+	        SecureRandom sr = new SecureRandom();
+	        Key secureKey = getKey(key);
+	        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+	        cipher.init(Cipher.ENCRYPT_MODE, secureKey, sr);
+	        byte[] bt = cipher.doFinal(data.getBytes());
+	        String strS = new BASE64Encoder().encode(bt);
+	        return strS;
+    	} catch(Exception e) {
+    		throw new RuntimeException("AES加密异常");
+    	}
+    }
+
+
+    public static String decode(String key, String data) {
+    	try {
+	        SecureRandom sr = new SecureRandom(); 
+	        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+	        Key secureKey = getKey(key);
+	        cipher.init(Cipher.DECRYPT_MODE, secureKey, sr);
+	        byte[] res = new BASE64Decoder().decodeBuffer(data);
+	        res = cipher.doFinal(res);
+	        return new String(res);
+	    } catch(Exception e) {
+			throw new RuntimeException("AES解密异常");
+		}
+    }
+    
+    
+    public static void main(String[] args) {
+    	String target = AES.encode("key", "test");
+    	System.out.println( target);
+    
+    }
+}
+
